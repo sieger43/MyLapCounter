6fb22916aadee9c2d8451e504587849e0803a787
John Berndt <johnwberndt@gmail.com>
2020-01-18 15:33:05 -0500

    feat: added first cut of the view controller for the Weather Forecast tab

---
+++ MyLapCounter/Controller/WeatherForecastViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherForecastViewController.swift
+//  MyLapCounter
+//
+//  Created by John Berndt on 1/18/20.
+//  Copyright © 2020 John Berndt. All rights reserved.
+//
+
+import UIKit
+
+class WeatherForecastViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
