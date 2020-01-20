f11ffb0fced519167edbb0113610eda22a190603
John Berndt <johnwberndt@gmail.com>
2020-01-19 22:10:20 -0500

    feat: Location is correct on using the "Update" button on the weather tab

--- MyLapCounter/Controller/WeatherForecastViewController.swift
+++ MyLapCounter/Controller/WeatherForecastViewController.swift
@@ -15,7 +15,12 @@ class WeatherForecastViewController: UIViewController {
     var lon : Double = 0.0
     
     @IBOutlet weak var locationValueLabel: UILabel!
+    @IBOutlet weak var temperatureValueLabel: UILabel!
+    @IBOutlet weak var conditionsValueLabel: UILabel!
     @IBOutlet weak var windValueLabel: UILabel!
+    @IBOutlet weak var humidityValueLabel: UILabel!
+    
+    @IBOutlet weak var lastUpdatedLabel: UILabel!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -33,6 +38,25 @@ class WeatherForecastViewController: UIViewController {
         
         lat = (UIApplication.shared.delegate as! AppDelegate).lat
         lon = (UIApplication.shared.delegate as! AppDelegate).lon
+        
+        let location : CLLocation = CLLocation(latitude: lat, longitude: lon)
+        
+        let geocoder = CLGeocoder()
+        
+        // Look up the location and pass it to the completion handler
+        geocoder.reverseGeocodeLocation(location,
+            completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    if let name = firstLocation?.name, let zip = firstLocation?.postalCode {
+                        self.locationValueLabel.text = name + " " + zip
+                    }
+                }
+                else {
+                    // An error occurred during geocoding.
+                    self.locationValueLabel.text = "unknown"
+                }
+        })
         
         OpenWeatherClient.getCurrentWeather(lat: lat, lon: lon){ success, error, response in
             if success {
